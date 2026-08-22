cdced8e44c73d2d0e8970d19ed135fefb979a704
oscar28duque <161505944+oscar28duque@users.noreply.github.com>
2026-08-22 17:15:54 -0500

    limites

--- Assets/Scripts/limite.cs
+++ Assets/Scripts/limite.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class limite : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Rigidbody2D rb;
+
+    [Header("Configuración de Límites")]
+    public Vector2 centroLimites = Vector2.zero;
+    public Vector2 tamanoLimites = new Vector2(10f, 10f);
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Calcular extremos de la caja
+        float minX = centroLimites.x - (tamanoLimites.x / 2f);
+        float maxX = centroLimites.x + (tamanoLimites.x / 2f);
+        float minY = centroLimites.y - (tamanoLimites.y / 2f);
+        float maxY = centroLimites.y + (tamanoLimites.y / 2f);
+
+        // Forzar la posición del Rigidbody dentro del rango
+        float posXClamped = Mathf.Clamp(rb.position.x, minX, maxX);
+        float posYClamped = Mathf.Clamp(rb.position.y, minY, maxY);
+
+        // Aplicar la posición segura usando MovePosition para no romper las físicas
+        rb.MovePosition(new Vector2(posXClamped, posYClamped));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(centroLimites, new Vector3(tamanoLimites.x, tamanoLimites.y, 0f));
     }
 }
+
+
