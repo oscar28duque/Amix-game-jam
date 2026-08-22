6bdbb502c150495df875ffa2c131a6da714b5318
JuanGuillermoSierraPoveda <jguillermosierra@ucundinamarca.edu.co>
2026-08-22 15:15:56 -0500

    bombillos prenden

--- Assets/Scripts/Bombillos.cs
+++ Assets/Scripts/Bombillos.cs
@@ -1,14 +1,33 @@
 using UnityEngine;
 
 public class Bombillos : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+{   
+    public Sprite bombilloEncendido;
+    private SpriteRenderer spriteRenderer;
+    private bool isEncendido = false;
+    
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!isEncendido && collision.GetComponent<OndaExpansiva>() != null)
+        {
+            Encender();
+        }
+    }
+    
+    private void Encender()
+    {
+        isEncendido = true;
+        if (bombilloEncendido != null)
+        {
+            spriteRenderer.sprite = bombilloEncendido;
+        }
+    }
+
     void Update()
     {
         
